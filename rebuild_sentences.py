9512555bc6b15a5bfc73117954d7e8eb3297f2e0
Alya Atlab <133522435+alya-atlab@users.noreply.github.com>
2024-11-04 12:37:26 +0200

    Add Comments

--- rebuild_sentences.py
+++ rebuild_sentences.py
@@ -1,13 +1,11 @@
 def rebuild_sentence(words , lengths):
-    sentences ="" 
+    sentences ="" #create a string to add the sentence
     for x , i in zip(words , lengths) : #to loop the two lists in parallel
         x = x[0 : i] #to slice the string [start : end]
-        print(x , i)
-        sentences += f"{x} "
-        
-        
+        sentences += f"{x} " # add the new string after slicing 
+            
     return sentences
         
         
-print(rebuild_sentence( [ "the", "sky", "is", "blue" ] , [ 3, 2, 2, 1 ] ))
+print(rebuild_sentence( [ "the", "sky", "is", "blue" ] , [ 3, 2, 2, 1 ] )) #for testing
         
