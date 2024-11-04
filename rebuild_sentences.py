e5be5d3ee06d769971b151866542582992a14a99
Alya Atlab <133522435+alya-atlab@users.noreply.github.com>
2024-11-04 12:17:40 +0200

    Add Return

--- rebuild_sentences.py
+++ rebuild_sentences.py
@@ -1,7 +1,10 @@
 def rebuild_sentence(words, lengths):
     for x , i in zip(words , lengths) :
-        x = x.index =lengths[i]
+        x = x[0:i]
+        print(x , i)
+        
+    return words
         
         
-rebuild_sentence( [ "the", "sky", "is", "blue" ] , [ 3, 2, 2, 1 ] )
+print(rebuild_sentence( [ "the", "sky", "is", "blue" ] , [ 3, 2, 2, 1 ] ))
         
