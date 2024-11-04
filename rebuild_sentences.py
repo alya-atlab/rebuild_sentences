8214fe3c92d9e5e1fdfeb34e5c00b18f94fe1618
Alya Atlab <133522435+alya-atlab@users.noreply.github.com>
2024-11-04 12:34:15 +0200

    Fix The Function

--- rebuild_sentences.py
+++ rebuild_sentences.py
@@ -1,9 +1,12 @@
-def rebuild_sentence(words, lengths):
-    for x , i in zip(words , lengths) :
-        x = x[0:i]
+def rebuild_sentence(words , lengths):
+    sentences ="" 
+    for x , i in zip(words , lengths) : #to loop the two lists in parallel
+        x = x[0 : i] #to slice the string [start : end]
         print(x , i)
+        sentences += f"{x} "
         
-    return words
+        
+    return sentences
         
         
 print(rebuild_sentence( [ "the", "sky", "is", "blue" ] , [ 3, 2, 2, 1 ] ))
